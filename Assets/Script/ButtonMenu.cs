6200a2e27da229f83782dc521ab5373ab8c627ea
Rostyk <grandbloodycf@gmail.com>
2017-06-06 10:43:18 +0300

    Homeworks

---
+++ Assets/Script/ButtonMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenu : MonoBehaviour {
+  
+
+    public void Settings()
+    {
+
+    }
+
+    public void GamePlay()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+
+}
